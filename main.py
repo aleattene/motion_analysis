4efaf33588d7a9d09fb4cac73dcab5882fc1a4a7
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2024-06-17 16:53:50 +0200

    chore(video): comment unused code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from motion_analysis.get_angles.get_elbow_shoulder_hip_knee_reverse_angles \
     import get_elbow_shoulder_hip_knee_reverse_angles
 from motion_analysis.get_angles.get_elbow_shoulder_hip_knee_double_angles \
     import get_elbow_shoulder_hip_knee_double_angles
-from motion_analysis.get_angles.get_body_rotation_angle import get_right_arm_rotation_angle
+# from motion_analysis.get_angles.get_body_rotation_angle import get_right_arm_rotation_angle
 
 
 def main():
@@ -37,19 +37,19 @@ def main():
 
     result01 = get_elbow_shoulder_hip_knee_angles(source01)
     print(result01)
-    convert_video_to_whatsapp_compatibility(source01[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source01[0:-4] + postfix_analyzed)
 
     result02 = get_elbow_shoulder_hip_knee_double_angles(source02)
     print(result02)
-    convert_video_to_whatsapp_compatibility(source02[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source02[0:-4] + postfix_analyzed)
 
     result03 = get_shoulder_hip_knee_ankle_angles(source03)
     print(result03)
-    convert_video_to_whatsapp_compatibility(source03[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source03[0:-4] + postfix_analyzed)
 
     result04 = get_elbow_shoulder_hip_knee_reverse_angles(source04)
     print(result04)
-    convert_video_to_whatsapp_compatibility(source04[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source04[0:-4] + postfix_analyzed)
 
     # result05 = get_right_arm_rotation_angle(source05)
     # print(result05)
@@ -57,15 +57,15 @@ def main():
 
     result06 = get_knee_ankle_foot_angle(source06)
     print(result06)
-    convert_video_to_whatsapp_compatibility(source06[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source06[0:-4] + postfix_analyzed)
 
     result07 = get_elbow_wrist_pinky_angle(source07)
     print(result07)
-    convert_video_to_whatsapp_compatibility(source07[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source07[0:-4] + postfix_analyzed)
 
     result08 = get_elbow_shoulder90_hip_knee_angles(source08)
     print(result08)
-    convert_video_to_whatsapp_compatibility(source08[0:-4] + postfix_analyzed)
+    # convert_video_to_whatsapp_compatibility(source08[0:-4] + postfix_analyzed)
 
 
 if __name__ == "__main__":
